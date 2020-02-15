cd10ed00cd9546bca7c9e91bb6a88fba50e50827
Ross Wang <rosswang@google.com>
2020-02-15 11:34:48 -0800

    Menu
    2D scene is the only one wired up right now.

---
+++ Assets/Scenes/SceneChanger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
