ecf22b2611f84a64f6abf9b11665d200ee327e55
Ross Wang <rosswang@google.com>
2020-02-16 15:44:41 -0800

    Ability to jump
    A short jump on space and middle mouse button.
    
    This is fun and makes much larger mazes more fun to solve.

--- Assets/Scenes/Controller3D2D.cs
+++ Assets/Scenes/Controller3D2D.cs
@@ -1,39 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Controller3D2D : MonoBehaviour
 {
-    public float Force, Torque;
+    public float Acceleration, Speed, Torque, JumpSpeed;
 
     private Rigidbody physics;
+    private float y0;
+    private bool jump = false;
 
-    void Start()
+    private bool Grounded => physics.position.y <= y0;
+
+    private void Start()
     {
         physics = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
+        y0 = physics.position.y;
     }
 
-    public void FixedUpdate()
+    private void Update()
     {
-        Vector3 force = new Vector3();
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(2)) && Grounded)
+            jump = true;
+    }
 
+    private void FixedUpdate()
+    {
         if (Input.GetKey(KeyCode.LeftArrow))
             physics.AddRelativeTorque(0, -Torque, 0);
         if (Input.GetKey(KeyCode.RightArrow))
             physics.AddRelativeTorque(0, Torque, 0);
-        
+
         physics.AddRelativeTorque(0, Torque * Input.GetAxis("Mouse X"), 0);
 
-        if (Input.GetKey(KeyCode.A))
-            --force.x;
-        if (Input.GetKey(KeyCode.D))
-            ++force.x;
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetMouseButton(0))
-            ++force.z;
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || Input.GetMouseButton(1))
-            --force.z;
+        if (Grounded)
+        {
+            Vector3 direction = new Vector3();
 
-        physics.AddRelativeForce(Force * force.normalized);
+            if (Input.GetKey(KeyCode.A))
+                --direction.x;
+            if (Input.GetKey(KeyCode.D))
+                ++direction.x;
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetMouseButton(0))
+                ++direction.z;
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || Input.GetMouseButton(1))
+                --direction.z;
+
+            Vector3 acceleration = Acceleration * (physics.rotation * direction.normalized);
+            if ((physics.velocity + acceleration * Time.fixedDeltaTime).sqrMagnitude > Speed * Speed)
+                acceleration = Vector3.ClampMagnitude(acceleration, Speed / physics.velocity.magnitude);
+
+            physics.AddForce(acceleration, ForceMode.Acceleration);
+
+            if (jump)
+            {
+                physics.AddForce(0, JumpSpeed, 0, ForceMode.VelocityChange);
+            }
+        }
+
+        jump = false;
     }
 }
