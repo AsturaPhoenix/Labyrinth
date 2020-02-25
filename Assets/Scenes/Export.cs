813e3d838e75caca356a6682e980099f0f26d939
Ross Wang <rosswang@google.com>
2020-02-24 20:26:35 -0800

    Add padding to export box
    Adds a padding of 4 to the export text box.

--- Assets/Scenes/Export.cs
+++ Assets/Scenes/Export.cs
@@ -8,6 +8,18 @@ public class Export : MonoBehaviour, Submenu
 
     MenuStack Submenu.Menu { set => Menu = value; }
 
+    private void Start()
+    {
+        var textComponent = Serialized.textComponent;
+        var settings = textComponent.GetGenerationSettings(new Vector2(Serialized.preferredWidth, Serialized.minHeight));
+        var gen = textComponent.cachedTextGeneratorForLayout;
+        var text = Serialized.text;
+        var rectTransform = Serialized.GetComponent<RectTransform>();
+        var textRect = textComponent.rectTransform.rect;
+        rectTransform.sizeDelta = new Vector2(gen.GetPreferredWidth(text, settings), gen.GetPreferredHeight(text, settings))
+            + textRect.min + rectTransform.rect.size - textRect.max;
+    }
+
     public void OnBack()
     {
         Menu.Pop();
