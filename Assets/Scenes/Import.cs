ee1b970ae06abd89b8b37ac064c23ac2255fa617
Ross Wang <imagipioneer@gmail.com>
2020-02-29 21:59:01 -0800

    Throttle Import layout to handle paste better
    Unity pastes one character at a time, which causes a hang as many text metrics operations are performed. Instead,
    schedule a layout for the next update.

--- Assets/Scenes/Import.cs
+++ Assets/Scenes/Import.cs
@@ -9,19 +9,27 @@ public class Import : MonoBehaviour, Submenu
 
     MenuStack Submenu.Menu { set => Menu = value; }
 
-    public void Layout()
+    private bool needsLayout = false;
+
+    public void InvalidateLayout() => needsLayout = true;
+
+    private void Update()
     {
-        Debug.Log("Layout");
-        var textComponent = Serialized.textComponent;
-        var settings = textComponent.GetGenerationSettings(new Vector2(Serialized.preferredWidth, Serialized.minHeight));
-        var gen = textComponent.cachedTextGeneratorForLayout;
-        var text = Serialized.text;
-        var textRect = textComponent.rectTransform.rect;
-        var rectTransform = Serialized.GetComponent<RectTransform>();
-        Vector2 padding = textRect.min + rectTransform.rect.size - textRect.max;
-        var layout = Serialized.GetComponent<LayoutElement>();
-        layout.preferredWidth = gen.GetPreferredWidth(text, settings) + padding.x;
-        layout.preferredHeight = gen.GetPreferredHeight(text, settings) + padding.y;
+        if (needsLayout)
+        {
+            var textComponent = Serialized.textComponent;
+            var settings = textComponent.GetGenerationSettings(new Vector2(Serialized.preferredWidth, Serialized.minHeight));
+            var gen = textComponent.cachedTextGeneratorForLayout;
+            var text = Serialized.text;
+            var textRect = textComponent.rectTransform.rect;
+            var rectTransform = Serialized.GetComponent<RectTransform>();
+            Vector2 padding = textRect.min + rectTransform.rect.size - textRect.max;
+            var layout = Serialized.GetComponent<LayoutElement>();
+            layout.preferredWidth = gen.GetPreferredWidth(text, settings) + padding.x;
+            layout.preferredHeight = gen.GetPreferredHeight(text, settings) + padding.y;
+
+            needsLayout = false;
+        }
     }
 
     public void OnImport()
