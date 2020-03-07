26781e8115376ead0de223c59513244163ee453d
Ross Wang <imagipioneer@gmail.com>
2020-03-07 12:35:42 -0800

    Menu scaling for high-DPI displays
    Combines DPI scaling with min size requirements.

---
+++ Assets/Scenes/MenuScaler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScaler : MonoBehaviour
+{
+    public float ReferenceDpi = 96;
+    public Vector2 MinSize;
+
+    private void Start()
+    {
+        int minDim = Mathf.Min(Screen.width, Screen.height);
+        GetComponent<CanvasScaler>().scaleFactor = Mathf.Min(
+            Screen.dpi / ReferenceDpi,
+            minDim / MinSize.x,
+            minDim / MinSize.y);
+    }
+}
